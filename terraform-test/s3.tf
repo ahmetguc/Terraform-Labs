f767590c0fbc2d6ddc320fef89c12854183b0b78
ahmetguc <68933712+ahmetguc@users.noreply.github.com>
2022-10-06 13:42:57 -0400

    added new terraform files

--- terraform-test/s3.tf
+++ terraform-test/s3.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
-
 resource "aws_s3_bucket" "my-bucket" {
   bucket = "ahmetguc-s3-test-bucket"
 
